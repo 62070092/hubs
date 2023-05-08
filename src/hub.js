79a09a1f59b266180bea55137c793942caf48331
Tanya Sisook <62070092@it.kmitl.ac.th>
2023-05-08 17:13:07 +0700

    trackpad

--- src/hub.js
+++ src/hub.js
@@ -139,6 +139,7 @@ import "./components/avatar-audio-source";
 import "./components/avatar-inspect-collider";
 import "./components/video-texture-target";
 import "./components/mirror";
+import "./components/trackpad";
 
 import ReactDOM from "react-dom";
 import React from "react";
